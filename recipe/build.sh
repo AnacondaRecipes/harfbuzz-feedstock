590e3d4394b953ea93deeabfdf1333222c11f6ab
Cheng H. Lee <clee@anaconda.com>
2021-03-09 20:37:27 -0600

    Bump version to 2.7.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,53 +7,48 @@ set -e
 # the build while this happens.
 find $PREFIX -name '*.la' -delete
 
-# CircleCI seems to have some weird issue with harfbuzz tarballs. The files
-# come out with modification times such that the build scripts want to rerun
-# automake, etc.; we need to run it ourselves since we don't have the precise
-# version that the build scripts embed. And the 'configure' script comes out
-# without its execute bit set. In a Docker container running locally, these
-# problems don't occur.
+# necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
+# meson needs this to determine where the g-ir-scanner script is located
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
-# Anaconda recipe maintainers: Do *NOT* run `autoreconf` as that breaks
-# configure's ability to properly configure gobject introspection.
-#autoreconf -vfi
-#chmod +x configure
 
-declare -a configure_extra_opts
+declare -a meson_extra_opts
+
+# conda-forge disables introspection when cross-compiling, but that isn't a
+# concern for defaults.
+meson_extra_opts=(-Dintrospection=enabled)
+
 case "${target_platform}" in
     linux-*)
         # Needed for libxcb when using CDT X11 packages
-        export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+        #export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
         ;;
     osx-64)
-        configure_extra_opts+=(--with-coretext=yes)
+        meson_extra_opts+=(-Dcoretext=auto)
         ;;
 esac
 
-./configure --prefix="${PREFIX}" \
-            --host=${HOST} \
-            --disable-gtk-doc \
-            --enable-static \
-            --with-cairo=yes \
-            --with-graphite2=yes \
-            --with-gobject=yes \
-            ${configure_extra_opts[@]}
+meson setup builddir \
+    --buildtype=release \
+    --default-library=both \
+    --prefix="${PREFIX}" \
+    -Dlibdir=lib \
+    -Dcairo=enabled \
+    -Dfontconfig=enabled \
+    -Dfreetype=enabled \
+    -Dglib=enabled \
+    -Dgobject=enabled \
+    -Dgraphite=enabled \
+    -Dicu=enabled \
+    -Dgdi=auto \
+    -Dtests=disabled \
+    -Ddocs=disabled \
+    -Dbenchmark=disabled \
+    ${meson_extra_opts[@]}
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
-# FIXME
-# OS X:
-# FAIL: test-ot-tag
-# Linux (all the tests pass when using the docker image :-/)
-# FAIL: check-c-linkage-decls.sh
-# FAIL: check-defs.sh
-# FAIL: check-header-guards.sh
-# FAIL: check-includes.sh
-# FAIL: check-libstdc++.sh
-# FAIL: check-static-inits.sh
-# FAIL: check-symbols.sh
-# PASS: test-ot-tag
-# make check
-make install
+ninja -v -C builddir -j ${CPU_COUNT}
+ninja -C builddir install -j ${CPU_COUNT}
 
 pushd "${PREFIX}"
   rm -rf share/gtk-doc
