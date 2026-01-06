7db9be31f821407027ed9c83adce2a57ea3780e9
vchychy <vchychyrko@anaconda.com>
2026-01-06 15:07:22 +0200

    harfbuzz 12.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,30 @@
 
 set -ex
 
-# ppc64le cdt need to be rebuilt with files in powerpc64le-conda-linux-gnu instead of powerpc64le-conda_cos7-linux-gnu. In the meantime:
-if [ "$(uname -m)" = "ppc64le" ]; then
-  cp --force --archive --update --link $BUILD_PREFIX/powerpc64le-conda_cos7-linux-gnu/. $BUILD_PREFIX/powerpc64le-conda-linux-gnu
-fi
-
-# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
-# process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens.
-find $PREFIX -name '*.la' -delete
-
-
 # necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
-# meson needs this to determine where the g-ir-scanner script is located
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
+# meson uses PKG_CONFIG_PATH to search when not cross-compiling and
+# PKG_CONFIG_PATH_FOR_BUILD when cross-compiling,
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
 # Make sure .gir files in $PREFIX are found
 export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share:$BUILD_PREFIX/share
+
+meson_config_args=(
+    -Dbenchmark=disabled
+    -Dcairo=enabled
+    -Dcoretext=auto
+    -Ddocs=disabled
+    -Dfreetype=enabled
+    -Dgdi=auto
+    -Dglib=enabled
+    -Dgobject=enabled
+    -Dgraphite2=enabled
+    -Dicu=enabled
+    -Dintrospection=enabled
+    -Dtests=disabled
+)
 
 if [ -n "$OSX_ARCH" ] ; then
     # The -dead_strip_dylibs option breaks g-ir-scanner here
@@ -26,53 +33,15 @@ if [ -n "$OSX_ARCH" ] ; then
     export LDFLAGS_LD="$(echo $LDFLAGS_LD |sed -e "s/-dead_strip_dylibs//g")"
 fi
 
-declare -a meson_extra_opts
-
-# conda-forge disables introspection when cross-compiling, but that isn't a
-# concern for defaults.
-meson_extra_opts=(-Dintrospection=enabled)
-
-case "${target_platform}" in
-    linux-*)
-        # Needed for libxcb when using CDT X11 packages
-        #export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
-        ;;
-    osx-*)
-        meson_extra_opts+=(-Dcoretext=auto)
-        ;;
-esac
-
-# see https://github.com/harfbuzz/harfbuzz/blob/4.3.0/meson_options.txt
+# NB: $MESON_ARGS sets buildtype, prefix, and libdir.
 meson setup builddir \
-    --buildtype=release \
+    ${MESON_ARGS} \
+    "${meson_config_args[@]}" \
     --default-library=both \
-    --prefix="${PREFIX}" \
-    --wrap-mode=nofallback \
-    --libdir="${PREFIX}/lib" \
-    --includedir=${PREFIX}/include \
-    --pkg-config-path="${PKG_CONFIG_PATH}" \
-    -Dbenchmark=disabled \
-    -Dcairo=enabled \
-    -Dchafa=disabled \
-    -Ddirectwrite=disabled \
-    -Ddocs=disabled \
-    -Dfreetype=enabled \
-    -Dgdi=disabled \
-    -Dglib=enabled \
-    -Dgobject=enabled \
-    -Dgraphite=enabled \
-    -Dgraphite2=enabled \
-    -Dicu=enabled \
-    -Dintrospection=enabled \
-    -Dtests=enabled \
-    "${meson_extra_opts[@]}"
-
+    --wrap-mode=nofallback
 ninja -v -C builddir -j ${CPU_COUNT}
-
-ninja -v -C builddir test
-
 ninja -C builddir install -j ${CPU_COUNT}
 
-pushd "${PREFIX}"
-  rm -rf share/gtk-doc
+pushd $PREFIX
+rm -rf share/gtk-doc
 popd
